8d94826bbb5d52af1e33e3b701f478425d65d142
Dias <dmustafinnn@gmail.com>
2021-05-08 02:40:50 +0600

    "Setting up MVC pattern for Classroom"

--- src/UniversitySimulator/model/ClassroomStrategy.java
+++ src/UniversitySimulator/model/ClassroomStrategy.java
@@ -1,10 +1,25 @@
 package UniversitySimulator.model;
 
-import java.util.HashMap;
+import UniversitySimulator.view.Classroom;
 
-public class ClassroomStrategy implements CampusStrategy{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ClassroomStrategy implements CampusStrategy {
     private double moneyAmount; //This is to try to simulate pass by reference.
     private HashMap<String, Double> itemMoney;
+    private double classGpa;
+    private String className;
+    private String professorName;
+
+    public ClassroomStrategy() {
+        this.className = null;
+        this.professorName = null;
+    }
 
     @Override
     public void interact(HashMap<String, Double> itemMoney, double money) {
@@ -20,5 +35,68 @@ public class ClassroomStrategy implements CampusStrategy{
     @Override
     public HashMap<String, Double> getItemMoney() {
         return this.itemMoney;
+    }
+
+    public class Lecture {
+        private String lectureTopic;
+        private String lectureMaterial;
+
+        public Lecture(String lectureTopic, String lectureMaterial) {
+            this.lectureTopic = lectureTopic;
+            this.lectureMaterial = lectureMaterial;
+        }
+
+    }
+
+    public class Homework {
+
+    }
+
+    public class Test {
+
+    }
+
+    public class Quiz {
+
+    }
+
+    public class ClassGrade {
+        private final Map<String, Double> gradeToScore = new HashMap<>();
+        private ArrayList<Double> hwGrades;
+        private ArrayList<Double> testGrades;
+        private ArrayList<Double> attendanceGrades;
+
+        public void classGrade() {
+            gradeToScore.put("A+", 4.33);
+            gradeToScore.put("A", 4.00);
+            gradeToScore.put("A-", 3.67);
+            gradeToScore.put("B+", 3.33);
+            gradeToScore.put("B", 3.00);
+            gradeToScore.put("B-", 2.67);
+            gradeToScore.put("C+", 2.33);
+            gradeToScore.put("C", 2.00);
+            gradeToScore.put("C-", 1.67);
+            gradeToScore.put("D+", 1.33);
+            gradeToScore.put("D", 1.00);
+            gradeToScore.put("F", 0.0);
+            gradeToScore.put("FX", 0.0);
+        }
+
+        public double getGpa() {
+            double hwTotal = 0;
+            double testTotal = 0;
+            double attendanceTotal = 0;
+
+            for (double i : hwGrades)
+                hwTotal += i;
+            for (double i : testGrades)
+                testTotal += i;
+            for (double i : attendanceGrades)
+                attendanceTotal += i;
+
+            classGpa = 0.6 * testTotal + 0.3 * hwTotal + 0.1 * attendanceTotal; // not done
+
+            return classGpa;
+        }
     }
 }
--- src/UniversitySimulator/view/Classroom.java
+++ src/UniversitySimulator/view/Classroom.java
@@ -17,85 +17,39 @@ public class Classroom extends JPanel {
     private static final int rectangleWidth = 850;
     private static final int rectangleHeight = 600;
 
-    private String className;
-    private String professorName;
-    private double classGpa;
+    JPanel classNamesPanel;
+    JButton[] classNames;
 
-    public Classroom(/*String className, String professorName*/) {
-        this.className = className;
-        this.professorName = professorName;
-    }
+    public Classroom() {
+        this.classNamesPanel = new JPanel();
+        this.classNamesPanel.setMaximumSize(new Dimension(200, 400));
+        this.classNamesPanel.setLayout(new BoxLayout(this.classNamesPanel, BoxLayout.Y_AXIS));
+        this.classNames = new JButton[5];
 
-    public class Lecture {
-        private String lectureTopic;
-        private String lectureMaterial;
-
-        public Lecture(String lectureTopic, String lectureMaterial) {
-            this.lectureTopic = lectureTopic;
-            this.lectureMaterial = lectureMaterial;
+        for(int i = 0; i < classNames.length; i++) {
+            this.classNames[i] = new JButton("Class " + i);
+            this.classNamesPanel.add(this.classNames[i]);
         }
-    }
 
-    public class Homework {
-        private ArrayList<String> quizQuestions;
+        this.add(classNamesPanel);
+        this.setSize(rectangleWidth, rectangleHeight);
+        this.setVisible(true);
 
-        public void assignHW() {
 
-        }
     }
 
 
-
-    public class ClassGrade {
-        private final Map<String, Double> gradeToScore = new HashMap<>();
-        private ArrayList<Double> hwGrades;
-        private ArrayList<Double> testGrades;
-        private ArrayList<Double> attendanceGrades;
-
-        public void classGrade() {
-            gradeToScore.put("A+", 4.33);
-            gradeToScore.put("A", 4.00);
-            gradeToScore.put("A-", 3.67);
-            gradeToScore.put("B+", 3.33);
-            gradeToScore.put("B", 3.00);
-            gradeToScore.put("B-", 2.67);
-            gradeToScore.put("C+", 2.33);
-            gradeToScore.put("C", 2.00);
-            gradeToScore.put("C-", 1.67);
-            gradeToScore.put("D+", 1.33);
-            gradeToScore.put("D", 1.00);
-            gradeToScore.put("F", 0.0);
-            gradeToScore.put("FX", 0.0);
-        }
-
-        public double getGpa() {
-            double hwTotal = 0;
-            double testTotal = 0;
-            double attendanceTotal = 0;
-
-            for (double i : hwGrades)
-                hwTotal += i;
-            for (double i : testGrades)
-                testTotal += i;
-            for (double i : attendanceGrades)
-                attendanceTotal += i;
-
-            classGpa = 0.6 * testTotal + 0.3 * hwTotal + 0.1 * attendanceTotal; // not done
-
-            return 0;
-        }
-    }
 
     /**
      * A frame of a classroom will be drawn here
      * @param g
      */
-    public void paintComponent (Graphics g){
+   /* public void paintComponent (Graphics g){
         super.paintComponent(g);
         //g.drawString(className, 10, 10);
         g.drawRect(100, 50, rectangleWidth, rectangleHeight);
 
-    }
+    }*/
 }
 
 
