ff938dd5a46dc95a4751971f9ad4b8a37979e83e
Matthew Fu <matthew.fu@sjsu.edu>
2021-05-07 02:30:45 -0700

    Changed campus frame.

--- src/UniversitySimulator/view/Campus.java
+++ src/UniversitySimulator/view/Campus.java
@@ -34,13 +34,25 @@ public class Campus extends JPanel{
 
         // draw the Classroom Box
         g.setFont(new Font("TimesRoman", Font.PLAIN, 25));
-        g.drawString("Classroom", 650, 150);
-        g.drawRect(600, 100, rectangleWidth, rectangleHeight);
+        g.drawString("Classroom", 650, 130);
+        g.drawRect(600, 80, rectangleWidth, rectangleHeight-50);
 
         // draw the Library Box
         g.setFont(new Font("TimesRoman", Font.PLAIN, 25));
-        g.drawString("Library", 950, 625);
-        g.drawRect(900, 600, rectangleWidth, rectangleHeight);
+        g.drawString("Library", 80, 90);
+        g.drawRect(40, 40, rectangleWidth, rectangleHeight);
+
+        g.drawString("Cafeteria", 640, 300);
+        g.drawString("Bookstore", 900, 300);
+        g.drawRect(620, 240, rectangleWidth+200, rectangleHeight-40);
+
+        g.drawLine(1000, 100, 1100, 100);
+        g.drawLine(1050, 150, 1050, 50);
+        g.drawOval(1000, 50, 100, 100);
+        g.drawString("N", 1042, 47);
+        g.drawString("E", 1103, 108);
+        g.drawString("S", 1042, 171);
+        g.drawString("W", 975, 108);
 
     }
 }
